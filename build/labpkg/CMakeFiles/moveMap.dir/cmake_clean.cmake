c8f63ce706d68a470f4a527e3cab8e1368321561
pluguezadam <pluguezadamp@gmail.com>
2018-04-10 14:40:16 -0700

    removinmg uneeded files

---
+++ build/labpkg/CMakeFiles/moveMap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moveMap.dir/src/moveMap.cpp.o"
+  "/home/adam/wsfinal/devel/lib/labpkg/moveMap.pdb"
+  "/home/adam/wsfinal/devel/lib/labpkg/moveMap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moveMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/labpkg/CMakeFiles/treasurelocator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/treasurelocator.dir/src/treasurelocator.cpp.o"
+  "/home/adam/wsfinal/devel/lib/labpkg/treasurelocator.pdb"
+  "/home/adam/wsfinal/devel/lib/labpkg/treasurelocator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/treasurelocator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
