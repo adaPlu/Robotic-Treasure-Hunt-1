fcb2be27ab8939e4d71d4acc650cb43d1957626c
pluguezadam <pluguezadamp@gmail.com>
2018-05-01 14:19:34 -0700

    Removed 360 , added points, not working yet

---
+++ build/scavenger_hunt/CMakeFiles/treasurelocater.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/treasurelocater.dir/src/treasurelocater.cpp.o"
+  "/home/adam/wsfinal/devel/lib/scavenger_hunt/treasurelocater.pdb"
+  "/home/adam/wsfinal/devel/lib/scavenger_hunt/treasurelocater"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/treasurelocater.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
