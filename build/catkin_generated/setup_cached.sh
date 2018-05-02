1479a7c5d0b2eedda623a81e2604a582d1afa866
pluguezadam <pluguezadamp@gmail.com>
2018-05-01 19:01:28 -0700

    Cleaned up code

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PATH="/opt/ros/kinetic/bin:/home/adam/bin:/home/adam/.local/bin:/home/adam/bin:/home/adam/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 export PWD="/home/adam/wsfinal/build"
--- src/scavenger_hunt/src/gototreasure.cpp
+++ src/scavenger_hunt/src/gototreasure.cpp
@@ -8,33 +8,25 @@ Uses Move base to move to a group of fixed map points designed to cover the enit
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-//#include "geometry_msgs/Pose2D.h"
-//#include <tf2/LinearMath/Quaternion.h>
-//#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <time.h>
 
-//void currentPoseCallBack(const geometry_msgs::PoseWithCovarianceStamped&msg);
 void serviceActivated();
 void serviceDone(const actionlib::SimpleClientGoalState& state,const move_base_msgs::MoveBaseResultConstPtr& result);
 void serviceFeedback(const move_base_msgs::MoveBaseFeedbackConstPtr& fb);
-//void scan360();
-void turn(double rel_angle, double ang_speed, bool isClockwise);
 void setGoal(double x, double y);
 
 
 ros::Publisher vel;
 geometry_msgs::Pose currentPose;
 move_base_msgs::MoveBaseGoal goal;
-bool STOP = true;
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "gototreasure");
   ros::NodeHandle nh;
-  //ros::Subscriber subCurrentpose = nh.subscribe("/amcl_pose", 10, &currentPoseCallBack);
   vel =nh.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel", 10);
   ros::Rate loop_rate(10);
 
@@ -46,7 +38,6 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
   
-  //turn(13, 1, 1);
 
   setGoal(0,2);
 
@@ -55,11 +46,11 @@ int main(int argc, char** argv){
   ac.waitForResult();
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
   }
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
   setGoal(8,2);
@@ -69,11 +60,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
   
 
@@ -84,11 +75,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
   
   setGoal(0,4);
@@ -98,11 +89,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
  
@@ -113,11 +104,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+		
   }
 
   
@@ -128,11 +119,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+		
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
   
@@ -160,11 +151,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
 
@@ -175,11 +166,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+		
   }
 
  
@@ -190,11 +181,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
   
@@ -205,11 +196,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
    
   setGoal(0,6);
@@ -219,11 +210,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+		
   }
   
   setGoal(0,4);
@@ -233,11 +224,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
 	setGoal(-8,4);
@@ -247,11 +238,11 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
 
   setGoal(-8,3);
@@ -261,17 +252,26 @@ int main(int argc, char** argv){
 	ac.waitForResult();
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
    			ROS_INFO("The base moved to goal!");
-			//turn(13, 1, 1);
+			
 	}
   else{
     		ROS_INFO("The base failed to move to goal for some reason.");
-			//turn(13, 1, 1);
+			
   }
   return 0;
 }
 
 
 //Functions
+void setGoal(double x, double y){
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+  goal.target_pose.pose.position.x = x;
+  goal.target_pose.pose.position.y = y;
+  goal.target_pose.pose.orientation.w = 1;
+}
+
+
 void serviceActivated() {
     ROS_INFO_STREAM("Service received goal");
 }
@@ -289,67 +289,3 @@ void serviceFeedback(const move_base_msgs::MoveBaseFeedbackConstPtr& fb) {
 		    fb->base_position.pose.position.y);
 }
 
-/*
-void currentPoseCallBack(const geometry_msgs::PoseWithCovarianceStamped&msg){
-  currentPose.position.x = msg.pose.pose.position.x;
-  currentPose.position.y = msg.pose.pose.position.y;
-  currentPose.position.z = msg.pose.pose.position.z;
-  currentPose.orientation.x = msg.pose.pose.orientation.x;
-  currentPose.orientation.y = msg.pose.pose.orientation.y;
-  currentPose.orientation.z = msg.pose.pose.orientation.z;
-  currentPose.orientation.w = msg.pose.pose.orientation.w;
-  return;
-}
-
-void scan360(){
-
-  ROS_INFO("Scanning goal area....");
-	geometry_msgs::Twist twist;
-	ros::Rate ratet(10);	
-	double t0 = ros::Time::now().toSec();	
-  twist.angular.z = 0.4;
-	double current_angle = 0.0;
-
-	do{
-	     vel.publish(twist);
-	     double t1 = ros::Time::now().toSec();
-       current_angle = 0.4 * (t1-t0) * 57.29;	
-	     ros::spinOnce();
-       ratet.sleep();
-	}while(current_angle < 360 && ros::ok());
-	twist.angular.z = 0;
-  vel.publish(twist);
-
-}
-*/
-void turn(double rel_angle, double ang_speed, bool isClockwise){
-    ROS_INFO("Scanning goal area....");
-	geometry_msgs::Twist twist;
-
-	if(isClockwise)
-		twist.angular.z = -abs(ang_speed);
-	else	
-		twist.angular.z = abs(ang_speed);
-	ros::Rate ratet(10);	
-	double t0 = ros::Time::now().toSec();	
-	double current_angle = 0.0;
-
-	do{
-	     vel.publish(twist);
-	     double t1 = ros::Time::now().toSec();
-             current_angle = ang_speed * (t1-t0);	
-	     ros::spinOnce();
-             ratet.sleep();
-	}while(current_angle < rel_angle && ros::ok());
-	twist.angular.z = 0;
-        vel.publish(twist);
-
-}
-
-void setGoal(double x, double y){
-  goal.target_pose.header.frame_id = "map";
-  goal.target_pose.header.stamp = ros::Time::now();
-  goal.target_pose.pose.position.x = x;
-  goal.target_pose.pose.position.y = y;
-  goal.target_pose.pose.orientation.w = 1;
-}
